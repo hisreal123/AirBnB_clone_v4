11dcfce917905a7ce868d006e0b702c62c5db7f8
hisreal123 <omosgoerge@gmail.com>
2024-08-10 19:17:08 +0100

    task 1

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -19,7 +19,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/0-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -41,7 +41,7 @@ def hbnb():
                            states=st_ct,
                            amenities=amenities,
                            places=places,
-                           cache_id=cache_id)
+                           cache_id=uuid.uuid4())
 
 
 if __name__ == "__main__":
